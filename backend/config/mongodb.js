5a147c0caa89c8f09ed9191dddb77aafee0a0d94
Perfecto Cayabyab <perfectocayabyab@gmail.com>
2025-04-17 04:46:14 +0800

    Test

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "latest-fashion", // this is the actual DB name
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
 
-    console.log("✅ MongoDB Connected to latest-fashion DB");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
-  }
-};
+    mongoose.connection.on('connected',()=> {
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/latest-fashion`)
+
+}
 
 export default connectDB;
